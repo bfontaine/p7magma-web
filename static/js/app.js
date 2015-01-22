a2b7b572c10eaf239129b4e895163a6f9fd5e39d
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-22 02:07:30 +0100

    course#followed filter added

--- static/js/app.js
+++ static/js/app.js
@@ -7,9 +7,20 @@ app.config(function($interpolateProvider) {
 
 app.controller('mgCourses', ['$scope', function rmSMSCtrl($scope) {
 
+  $scope.followed_only = true;
+
+  $scope.courses = function() {
+    return $.grep($scope._courses || [], function(c) {
+      // filters
+      if ($scope.followed_only) { return c.followed; }
+
+      return true;
+    });
+  };
+
   // init
   $(function() {
-    $scope.courses = JSON.parse($('#courses-json').remove().html());
+    $scope._courses = JSON.parse($('#courses-json').remove().html());
     $scope.$apply();
   });
 
