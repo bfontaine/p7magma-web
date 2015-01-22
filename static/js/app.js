6a9178393a577388201496054468fef560d09fe3
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-22 12:07:49 +0100

    M1 support

--- static/js/app.js
+++ static/js/app.js
@@ -7,10 +7,65 @@ app.config(function($interpolateProvider) {
 
 app.controller('mgCourses', ['$scope', function rmSMSCtrl($scope) {
 
+  _scope = $scope; // debug
+
+  $.extend(true, $scope, {
+    fields: [
+      {
+        'id': 'code',
+        'label': 'Code',
+      },
+      {
+        'id': 'title',
+        'label': 'title',
+      },
+      {
+        'id': 'semester',
+        'label': 'Semestre',
+      },
+      {
+        'id': 'status',
+        'label': 'Statut',
+      },
+      {
+        'id': 'ects',
+        'label': 'ECTS',
+      },
+      {
+        'id': 'followed',
+        'label': 'Suivi ?',
+        'fmt': function(v) {
+          return v ? "oui" : "non";
+        }
+      },
+      {
+        'id': 'result',
+        'label': 'Note',
+        'fmt': function(v, c) { return c.session ? ("" + v + "/20") : "";  }
+      },
+      {
+        'id': 'jury',
+        'label': 'Note Jury',
+        'fmt': function(v, c) { return c.jury > 0 ? ("" + v + "/20") : "";  }
+      },
+      {
+        'id': 'session',
+        'label': 'Session',
+      },
+    ],
+
+    // filters
+    followed_only: true,
+
+    // late initialization
+    _courses: [],
+    available_fields: {},
+  });
+
   $scope.followed_only = true;
 
   $scope.courses = function() {
-    return $.grep($scope._courses || [], function(c) {
+    return $.grep($scope._courses, function(c) {
       // filters
       if ($scope.followed_only) { return c.followed; }
 
@@ -18,9 +73,29 @@ app.controller('mgCourses', ['$scope', function rmSMSCtrl($scope) {
     });
   };
 
+  $scope.setCourses = function(cs) {
+    $scope._courses = cs || [];
+
+    if ($scope._courses.length) {
+      $.each(cs[0], function(k, v) {
+        $scope.available_fields[k] = true;
+      });
+    }
+  };
+
+  // filter
+  $scope.displayedField = function(field) {
+    return $scope.available_fields.hasOwnProperty(field.id);
+  };
+
+  // format
+  $scope.fmtField = function(fn, value, course) {
+    return fn ? fn(value, course) : value;
+  };
+
   // init
   $(function() {
-    $scope._courses = JSON.parse($('#courses-json').remove().html());
+    $scope.setCourses(JSON.parse($('#courses-json').remove().html()));
     $scope.$apply();
   });
 
